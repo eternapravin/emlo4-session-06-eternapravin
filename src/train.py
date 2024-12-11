df4d9d9b48d4e1d52940510a765a3ac36d68cfec
eternapravin <pravinpraba@eternascisense.com>
2024-12-11 12:54:54 +0000

    updated the secret key in secrets in dvc-pipeline, created comet.yaml file and updated train.py to reflect comet ml specific to pytorch lighining

--- src/train.py
+++ src/train.py
@@ -13,13 +13,7 @@ from typing import Any, Dict
 import rootutils
 
 from comet_ml import start
-from comet_ml.integration.pytorch import log_model
-
-experiment = start(
-  api_key="zqyBU4kfjfxEqs5LEd4L2S9gr",
-  project_name="general",
-  workspace="eternapravin"
-)
+from comet_ml.integration.pytorch import CometLogger
 
 # Setup root directory
 root = rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
